9c70c49e8f957bcedd4e35e2db456a37bde53034
unknown <nicmartinez@alumnos.uai.cl>
2019-04-01 16:48:44 -0300

    Puse el getprocs

--- cuenta.c
+++ cuenta.c
@@ -3,5 +3,5 @@
 #include "user.h"
 
 int main(void) {
- printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+ printf("La cantidad de procesos en ejecucion en la CPU es %p\n", getprocs());
 }
