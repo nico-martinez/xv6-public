1edc1e0446b800eb906bd9bc7daa42e1c4af630f
unknown <nicmartinez@alumnos.uai.cl>
2019-06-16 16:37:28 -0400

    ahora chi trad

--- cuenta.c
+++ cuenta.c
@@ -5,6 +5,6 @@
 int main(int argc,char *argv[])
 {
     char *va =argv[1];
-    printf(1,"\n Direccion fisica: %s",(char*) traducir(va));
+    printf(1,"\n Direccion fisica: %d", traducir(va));
     exit();
 }
